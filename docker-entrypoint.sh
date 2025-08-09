c1dfb80fe1096d620cbe00815ab3c7cce65c286f
zaifin <zaifin@rediffmail.com>
2025-08-09 21:47:42 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "Running migrations..."
-python manage.py migrate sites --noinput
 python manage.py migrate --noinput
 
-echo "Populating DB and creating superuser..."
-python manage.py populatedb --createsuperuser
+echo "Populating DB and creating superuser"
+if [ "$(python manage.py showmigrations | grep '\[ \]' | wc -l)" -gt 0 ]; then
+    python manage.py populatedb --createsuperuser
+fi
 
 echo "Updating site info..."
 python manage.py update_site --domain "${ALLOWED_HOSTS}" --name "Zaifin Eco"
@@ -14,4 +15,5 @@ python manage.py update_site --domain "${ALLOWED_HOSTS}" --name "Zaifin Eco"
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+echo "Starting server..."
 exec "$@"
